00cb20fe429a3a753dad4769802f2a1faf8b12da
Mahyar Mohammdi <lilmahyar@protonmail.com>
2022-02-12 21:16:38 +0330

    bugs controller added

---
+++ BugTracker/Controllers/BugsController.cs
@@ -0,0 +1,28 @@
+﻿using BugTracker.Models;
+using BugTracker.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BugsController : Controller
+    {
+        private readonly TrackerService _trackerService;
+        public BugsController(TrackerService trackerService)
+        {
+            _trackerService = trackerService; 
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddNewBug(Bug bug)
+        {
+            await _trackerService.AddNewBugAsync(bug);
+            return Ok();
+        }
+    }
+}
