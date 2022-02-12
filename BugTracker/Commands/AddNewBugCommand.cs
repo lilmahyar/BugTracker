98d244225cb9357fd5d9852a6a9a0054e9f0089f
Mahyar Mohammdi <lilmahyar@protonmail.com>
2022-02-12 21:41:38 +0330

    adding new command

---
+++ BugTracker/Commands/AddNewBugCommand.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Commands
+{
+    public class AddNewBugCommand
+    {
+        [Required(ErrorMessage = "ایمیل الزامی میباشد")]
+        public string Email { get; set; }
+        [Required]
+        public string BugTitle { get; set; }
+        [Required]
+        public string BugDescription { get; set; }
+
+        public FixPriority Priority { get; set; }
+    }
+}
