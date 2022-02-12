022198f12f8822547ed52be7e38ccc0230c2d133
Mahyar Mohammdi <lilmahyar@protonmail.com>
2022-02-12 21:17:17 +0330

    new bugs type

---
+++ BugTracker/Models/FixPriority.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public enum FixPriority
+    {
+        [Description("فوری ")] Immediate = 1,
+
+        [Description("کم ")] Low = 2,
+        [Description("متوسط  ")] Medium = 4,
+        [Description("بالا  ")] High = 3
+
+    }
+}
