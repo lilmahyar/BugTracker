27d89343132a25d8d30cd2738ebd397274d8eae2
Mahyar Mohammdi <lilmahyar@protonmail.com>
2022-02-12 21:42:08 +0330

    writing command to model mapper

---
+++ BugTracker/Mapper/CommandToModelMapper.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Commands;
+using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Mapper
+{
+    public static class CommandToModelMapper
+    {
+        public static Bug ToModel(this AddNewBugCommand command)
+        {
+            return new Bug(command.Email, command.BugTitle, command.BugDescription);
+
+        }
+    }
+}
