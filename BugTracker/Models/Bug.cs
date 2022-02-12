6aeada25c1124771d0b804d230cef3d62e4ccfe3
Mahyar Mohammdi <lilmahyar@protonmail.com>
2022-02-12 21:16:49 +0330

    bugs entity

---
+++ BugTracker/Models/Bug.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class Bug
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "ایمیل الزامی میباشد")]
+        public string Email { get; set; }
+        [Required]
+        public string BugTitle { get; set; }
+        [Required]
+        public string  BugDescription { get; set; }
+
+
+        public FixPriority Priority { get; set;  }
+    }
+}
